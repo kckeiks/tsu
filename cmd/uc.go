41d555112207efec0763773143337c0c86b38c98
Miguel <kckeiks+github@gmail.com>
2021-10-11 21:32:39 -0400

    gofmt uc file

--- cmd/uc.go
+++ cmd/uc.go
@@ -4,18 +4,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	// Used for flags.
-	cfgFile     string
-	userLicense string
-	rootCmd     = &cobra.Command{
-		Use:          "uc",
-		Short:        "Unicode Converter",
-		SilenceUsage: true,
-	}
-)
+var rootCmd = &cobra.Command{
+	Use:          "uc",
+	Short:        "Unicode Converter",
+	SilenceUsage: true,
+}
 
-// Execute executes the root command.
 func Execute() error {
 	return rootCmd.Execute()
 }
